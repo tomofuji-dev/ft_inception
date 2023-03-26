0121719870d9157bfe0be221ee1155bb3a55951f
tfuji <tomofuji.dev@gmail.com>
2023-03-26 12:59:57 +0900

    add .env

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 set -eu -o pipefail
+# wp config create \
+# 	--dbname="${DB_NAME}" \
+# 	--dbuser="${DB_USER}" \
+# 	--dbpass="${DB_PASS}" \
+# 	--dbhost=mariadb:3306 \
+# 	--path="${WP_PATH}" \
+# 	--allow-root
+# wp db create
+# wp core install \
+# 	--url="${WP_URL}" \
+# 	--title="${WP_TITLE}" \
+# 	--admin_user="${WP_ADMIN_USER}" \
+# 	--admin_password="${WP_ADMIN_PASS}" \
+# 	--admin_email="${WP_ADMIN_EMAIL}" \
+# 	--path="${WP_PATH}" \
+# 	--allow-root
+# wp user create \
+# 	"${WP_EDITOR_USER}" \
+# 	"${WP_EDITOR_EMAIL}" \
+# 	--user_pass="${WP_EDITOR_PASS}" \
+# 	--role=editor \
+# 	--path="${WP_PATH}" \
+# 	--allow-root
 exec "$@"
